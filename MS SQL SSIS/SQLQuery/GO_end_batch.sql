2c7e113d949e95e35b80f8df0dfd1d184f8bd28b
Emmanuel Orubele <blontf@gmail.com>
2025-02-20 07:15:47 -0500

    T-SQL variables

--- MS SQL SSIS/SQLQuery/GO_end_batch.sql
+++ MS SQL SSIS/SQLQuery/GO_end_batch.sql
@@ -23,3 +23,27 @@ CREATE TABLE tblSecond
 	myNumbers int);
 
  insert into tblFirst values(234), (456),(888)
+
+ ---How to create variables in T-SQL
+ 
+-- Initialise a variable, give it a data type and initial value
+DECLARE @myvar AS smallint = 2000
+
+-- Increase that value by 1
+SET @myvar = @myvar * 10
+
+SELECT @myvar AS myVariable
+
+Bigint
+Int up to 2,000,000,000
+tinyint - 0-255
+smallint - -32767 to 32768
+
+
+--BITS = Bigint Int Tinyint Smallint
+
+DECLARE @var AS tinyint = 32767
+
+SET @var = 20000
+
+SELECT @var AS myvariable
